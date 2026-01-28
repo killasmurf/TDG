9da2b1b3b97aa1093d2d99ebe5bf456d2f13c16e
killasmurf <adam__murphy@hotmail.com>
2026-01-28 22:52:32 +1100

    Add WaveManager unit tests

---
+++ tests/waveManager.test.js
@@ -0,0 +1,36 @@
+import WaveManager from '../src/core/waveManager.js';
+import EntityManager from '../src/core/entityManager.js';
+
+class MockEntityManager {
+  constructor() {
+    this.enemies = [];
+    this.path = [{x:0, y:0}];
+  }
+  spawnEnemy(type, path) {
+    this.enemies.push({type, path, active:true});
+  }
+}
+
+describe('WaveManager', () => {
+  const waves = [
+    {
+      name: 'Test',
+      enemies: [{type:'basic', count:2, interval:1}]
+    }
+  ];
+  const mockEm = new MockEntityManager();
+  const wm = new WaveManager(mockEm, waves);
+
+  test('starts next wave', () => {
+    expect(wm.startNextWave()).toBe(true);
+    expect(wm.isWaveActive).toBe(true);
+  });
+
+  test('spawns enemies over time', () => {
+    wm.startNextWave();
+    for(let t = 0; t < 3000; t+=1000){
+       wm.update(1000);
+    }
+    expect(mockEm.enemies.length).toBe(2);
+  });
+});
